d212a8c6815c2891fc002b9bd6fea3d2f001aade
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-08-10 19:05:57 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/chaehyeon/study_activity/MainActivity.java
@@ -0,0 +1,15 @@
+package kr.hs.emirim.chaehyeon.study_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {  //onCreate 가장 먼저 실행되는 것
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
