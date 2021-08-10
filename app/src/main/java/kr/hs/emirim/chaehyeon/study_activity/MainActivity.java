be46f856d00ce9aa4d2be1c00af335496afda922
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-08-10 19:12:04 +0900

    Edit 7개 메서드 오버라이딩(부가 설명) Commit

--- app/src/main/java/kr/hs/emirim/chaehyeon/study_activity/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/study_activity/MainActivity.java
@@ -12,6 +12,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //앱 실행
+        //onCreate(0 --> 처음 앱을 실행시켰을때 단 한번 호출되는 메서드
+        //onStart()
+        //onResume()
+
+        //홈 버튼 클릭
+        //onPause()
+        //onStop()
+
+        //다시 앱으로 복귀
+        //onRestart()
+        //onStart()
+        //onResume()
+
+        //뒤로가기 버튼을 통해서 앱을 완전히 종료
+        //onPause()
+        //onStop()
+        //onDestroy() --> 앱이 완전히 종료되었을때 딱 한번 호출
+
+
         Log.i("MY", "__ onCreate() --"); //Log는 콘솔에 찍는게 아닌 Lotcat에 찍히는 디버깅 코드
     }//oncreate()
 
